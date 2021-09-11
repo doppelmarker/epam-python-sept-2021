7768721900cbdc87546a3c634e5aeae07a25b656
doppelmarker <doppelmarker@gmail.com>
2021-09-12 00:50:13 +0300

    correct task1_8 (separate header row)

--- homework1/task1_8/pretty_print/pretty_print.py
+++ homework1/task1_8/pretty_print/pretty_print.py
@@ -1,12 +1,15 @@
 def print_multiplication_table(a, b, c, d):
-    for row_idx, row_val in enumerate(range(a - 1, b + 1)):
+    print(f"{'':7}", end="")
+    for i in range(c, d + 1):
+        print(f"{i:7}", end="")
+    print("\n")
+
+    for row_idx, row_val in enumerate(range(a, b + 1)):
         for col_idx, col_val in enumerate(range(c, d + 1)):
-            if row_idx == col_idx == 0:
-                print(f"{'':7}", end="")
-            elif col_idx == 0:
-                print(f"{range(a, b + 1)[row_idx - 1]:7}", end="")
+            if col_idx == 0:
+                print(f"{range(a, b + 1)[row_idx]:7}", end="")
             print(f"{row_val * col_val:7}", end="")
         print("\n")
 
 
-print_multiplication_table(2, 4, 3, 7)
+print_multiplication_table(2, 4, 5, 7)
