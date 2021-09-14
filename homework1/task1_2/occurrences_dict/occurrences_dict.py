3b8bea637405f75759bd148fbf45173f5473ba90
doppelmarker <doppelmarker@gmail.com>
2021-09-14 18:34:30 +0300

    add task description

--- homework1/task1_2/occurrences_dict/occurrences_dict.py
+++ homework1/task1_2/occurrences_dict/occurrences_dict.py
@@ -1,3 +1,14 @@
+"""
+### Task 1.2
+Write a Python program to count the number of characters (character frequency) in a string (ignore case of letters).
+Examples:
+```
+Input: 'Oh, it is python'
+Output: {',': 1, ' ': 3, 'o': 2, 'h': 2, 'i': 2, 't': 2, 's': 1, 'p': 1, 'y': 1, 'n': 1}
+```
+"""
+
+
 def get_occurrences_dict(s):
     d = dict()
     for char in s.lower():
