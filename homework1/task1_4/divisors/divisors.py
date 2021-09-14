e2efe2e79835c26e6369a90014996a9fa38ba630
doppelmarker <doppelmarker@gmail.com>
2021-09-14 18:36:05 +0300

    add task description

--- homework1/task1_4/divisors/divisors.py
+++ homework1/task1_4/divisors/divisors.py
@@ -1,2 +1,13 @@
+"""
+### Task 1.4
+Create a program that asks the user for a number and then prints out a list of all the [divisors](https://en.wikipedia.org/wiki/Divisor) of that number.
+Examples:
+```
+Input: 60
+Output: {1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60}
+```
+"""
+
+
 def find_all_divisors(n):
     return {num for num in range(n // 2, 0, -1) if n % num == 0} | {n}
