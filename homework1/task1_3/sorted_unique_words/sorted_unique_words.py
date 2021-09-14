8b1038698cf7090ec643fdf789de2224d03b3b41
doppelmarker <doppelmarker@gmail.com>
2021-09-14 18:35:11 +0300

    add task description

--- homework1/task1_3/sorted_unique_words/sorted_unique_words.py
+++ homework1/task1_3/sorted_unique_words/sorted_unique_words.py
@@ -1,2 +1,13 @@
+"""
+### Task 1.3
+Write a Python program that accepts a comma separated sequence of words as input and prints the unique words in sorted form.
+Examples:
+```
+Input: ['red', 'white', 'black', 'red', 'green', 'black']
+Output: ['black', 'green', 'red', 'white']
+```
+"""
+
+
 def get_sorted_unique_words(sequence):
     return type(sequence)(sorted(set(sequence)))
