17fb661ad377ddcc176d09743625cd4caa70c677
doppelmarker <doppelmarker@gmail.com>
2021-09-14 18:38:21 +0300

    add task description

--- homework1/task1_8/pretty_print/pretty_print.py
+++ homework1/task1_8/pretty_print/pretty_print.py
@@ -1,3 +1,23 @@
+"""
+### Task 1.8
+Write a program which makes a pretty print of a part of the multiplication table.
+Examples:
+```
+Input:
+a = 2
+b = 4
+c = 3
+d = 7
+
+Output:
+	3	4	5	6	7
+2	6	8	10	12	14
+3	9	12	15	18	21
+4	12	16	20	24	28
+```
+"""
+
+
 def print_multiplication_table(a, b, c, d):
     print(f"{'':7}", end="")
     for i in range(c, d + 1):
