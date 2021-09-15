667274855ab1bf5e7f7629f181f661bd679eed4d
doppelmarker <doppelmarker@gmail.com>
2021-09-15 11:43:07 +0300

    replace solution with pretty one-liner

--- homework1/task1_2/occurrences_dict/occurrences_dict.py
+++ homework1/task1_2/occurrences_dict/occurrences_dict.py
@@ -12,8 +12,5 @@ Output: {',': 1, ' ': 3, 'o': 2, 'h': 2, 'i': 2, 't': 2, 's': 1, 'p': 1, 'y': 1,
 def get_occurrences_dict(s):
     d = dict()
     for char in s.lower():
-        if char not in d:
-            d[char] = 1
-        else:
-            d[char] += 1
+        d[char] = d.get(char, 0) + 1
     return d
